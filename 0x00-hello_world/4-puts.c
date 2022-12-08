3c2de06a3d7d384ecfc10c69d416ec58f28977e0
Reginald Amuzu <reginaldrock219@gmail.com>
2022-12-08 07:34:31 -0800

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,19 +3,15 @@
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return - Always 0 (Success)
+ */
 
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
