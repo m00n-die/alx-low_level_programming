601958ff71aceba4f7e61cef6fd15bd5fcad696c
Reginald Amuzu <reginaldrock219@gmail.com>
2022-12-09 07:46:40 -0800

    Added some files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwyas 0 (Success)
+ */
+
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+		putchar(lower);
+			putchar('\n');
+
+	return (0);
+}
