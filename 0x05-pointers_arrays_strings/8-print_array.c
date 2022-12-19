3c822744293daa803619b537f5a2fc98a5e0b8da
Reginald Amuzu <reginaldrock219@gmail.com>
2022-12-19 09:51:01 -0800

    Fixed 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,13 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	for (; i < n; i++)
-	{
-		printf("%d", a[i]);
-		if (i < n - 1)
-			printf(", ");
-	}
-	_putchar('\n');
+	for (i = 0; i < n; i++)
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+
+	printf("\n");
 }
