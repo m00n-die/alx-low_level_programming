8875d8d572d70dffabe00f28a12610cf5019400b
Reginald Amuzu <reginaldrock219@gmail.com>
2023-01-03 09:56:38 -0800

    Major Fixes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strpbrk -  a function that searches a string for any of a set of bytes.
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strstr -  a function that locates a substring
