2bbf36226545428bae63dd004a3f15400ec57766
reginaldrock219@gmail.com <m00n-die>
2023-05-25 03:50:37 -0700

    All mandatory tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""function def island_perimeter(grid): that returns the
+perimeter of the island described in grid
+"""
+
+
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,1 @@
+ALl tasks under 0x1C-makefiles in the ALX SE program.
