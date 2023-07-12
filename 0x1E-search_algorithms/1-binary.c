9aa0b5c7a20e0099623383fcc5f496db33d5b7e8
m00n-die <reginaldrock219@gmail.com>
2023-07-12 06:05:28 +0100

    Fixed 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,54 @@
 #include "search_algos.h"
+
 /**
-  * binary_search - implements a binary serach algorithm
-  * @array: ordered array of numbers to be sorted
+  * recursiveBinarySearch - implements a recursive binary search algorithm
+  * @array: ordered array to be searched
+  * @left: start of the array
+  * @right: end of the array
+  * @element: element being searched for
+  */
+
+int recursiveBinarySearch(int *array, size_t left, size_t right, int element)
+{
+	size_t i = 0;
+
+
+	if (array == NULL || right == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < right; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	printf("\n");
+
+	if (left <= right)
+	{
+		size_t middle = (left + (right - left) / 2);
+
+		if (array[middle] == element)
+			return (middle);
+		if (array[middle] > element)
+			return (recursiveBinarySearch(array, left, middle - 1, element));
+		return (recursiveBinarySearch(array, middle + 1, right, element));
+	}
+	return (-1);
+}
+
+/**
+  * binary_search - implements a binary search algorithm
+  * @array: ordered array to be searched
   * @size: size of array
-  * @value: value being searche for
+  * @value: value to eb searched for
   *
-  * Return: index of value if succesful else -1
+  * Return: index on success
   */
 
 int binary_search(int *array, size_t size, int value)
 {
-	int left = 0;
-	int right = size;
-	int mid;
-	int i = 0;
+	int index = recursiveBinarySearch(array, 0, size, value);
 
-	if (!array || size == 0)
+	if (index == -1)
 		return (-1);
-
-	for (left = 0; left < right; left++)
-	{
-		printf("Searching in array: ");
-
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-
-		mid = round((left + right) * 2);
-		if (value == array[mid])
-			return (mid);
-		else if (value < array[mid])
-			mid = mid - 1;
-		else if (value > array[mid])
-			mid = mid + 1;
-	}
-	return (-1);
+	return (index);
 }
